90a583837e5b7ee7bef7f4f077a3af0234c6796b
Ksnarf <44852455+Ksnarf@users.noreply.github.com>
2022-01-18 09:49:06 -0800

    Initial commit
    qbcore + base gta files.

---
+++ resources/[FiveM-NoahChat-main]/README.md
@@ -0,0 +1,32 @@
+> # FiveM-NoahChat #
+> This is Noahs Version of ingame FiveM Chat!
+> 
+> **Features:**
+> - Toggle Chat Visibility 
+> - Clear Chat
+> - Suggestion bar
+> - Clear Text Bar
+> - Overall clean look
+> 
+> Basic Clear Chat with Command
+> 
+> > ![alt text](https://cdn.discordapp.com/attachments/779842019723313162/800854901080064020/unknown.png)
+> 
+> Basic Command Being Used
+> 
+> > ![alt text](https://cdn.discordapp.com/attachments/779842019723313162/800854941321003028/unknown.png)
+> 
+> > **Commands**
+> /togglechat
+> 
+> **Optional**
+> These are optional messages you can include to your sv_chat.lua for messages welcoming indivuals to your server. To add them copy and paste and fill in your server name where it says server name. The seccond message is a leave message.
+> 
+> 
+> AddEventHandler('chat:init', function()
+>     TriggerClientEvent('chatMessage', -1, '', { 255, 255, 255 }, '^3Welcome | ^0' .. GetPlayerName(source) .. '^3 to INSERT SERVER NAME')
+> end)
+> 
+> AddEventHandler('playerDropped', function(reason)
+>     TriggerClientEvent('chatMessage', -1, '', { 255, 255, 255 }, '^3Player^0: ' .. GetPlayerName(source) ..' Disconnected Reason: (' .. reason .. ')')
+> end)
---
+++ resources/[dvandcar-newton]/client.lua
@@ -0,0 +1,54 @@
+RegisterCommand('car', function(source, args)
+    local vehicleName = args[1] or 'adder'
+    if not IsModelInCdImage(vehicleName) or not IsModelVehicle(VehicleName) then
+        triggerEvent{'chat:addmessage', {
+            args = ('Vehicle not recognised: ' .. vehicleName)
+
+        }}
+        return 
+    end
+
+    RequestModel(vehicleName)
+
+    while not HasModelLoaded(vehicleName) do
+        Wait(500)
+    end
+
+    local playerPed = PlayerPedID()
+    local pos = GetEntityCoords(playerPed)
+
+    local vehicle = CreateVehicle(vehicleName, pos.x, pox.y, pos.z, GetEntityHeading(playerPed), true, false)
+
+    SetPedIntoVehicle(playerPed, vehicle, -1)
+
+    SetEntityAsNoLongerNeeded(vehicle)
+
+    SetModelAsNoLongerNeeded(vehicleName)
+
+    TriggerEvent('chat:addMessage', {
+        args = {'You have spawned a ' .. vehicleName .. '.'}  
+    }}
+end, false) 
+
+RegisterCommand('dv', function()
+    local playerPed = PlayerPedId()
+
+    local vehicle = GetVehiclePedIsIn(playerPed, false)
+    
+    DeleteEntity(vehicle)
+end, false)
+
+RegisterCommand('dv', function()
+    local playerPed = PlayerPedId()
+
+    local vehicle = GetVehiclePedIsIn(playerPed, false)
+
+    SetPedIntoVehicle(playerPed, vehicle, -1)
+
+end, false)
+
+
+
+
+
+

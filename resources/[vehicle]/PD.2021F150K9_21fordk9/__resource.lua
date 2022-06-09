247ab8a18f3a345d177ff2cfd67624cb972ecb1d
Ksnarf <44852455+Ksnarf@users.noreply.github.com>
2022-06-09 10:33:07 -0700

    21f150 file mod

--- resources/[vehicle]/PD.2021F150K9_21fordk9/__resource.lua
+++ resources/[vehicle]/PD.2021F150K9_21fordk9/__resource.lua
@@ -2,11 +2,11 @@ resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
 
 files {
 	'els/21f150k9.xml',
-	'data/vehicles.meta',
-	'data/carvariations.meta'
+	'vehicles.meta',
+	'carvariations.meta'
 }
 
-data_file 'VEHICLE_METADATA_FILE' 'data/vehicles.meta'
-data_file 'CARVARIATIONS_METADATA_FILE' 'data/carvariations.meta'
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+data_file 'CARVARIATIONS_METADATA_FILE' 'carvariations.meta'
 
 is_els 'true'

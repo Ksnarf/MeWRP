f64fda76d8bc65d47eb227bec0bc1bd2d2bf1da5
Ksnarf <44852455+Ksnarf@users.noreply.github.com>
2022-06-10 20:36:42 -0700

    dammit.. again

--- resources/[qb]/qb-vehicleshop/client.lua
+++ resources/[qb]/qb-vehicleshop/client.lua
@@ -32,7 +32,7 @@ end)
 -- Static Headers
 local vehHeaderMenu = {
     {
-        header = en:t('menus.vehHeader_header'),
+        header = Lang:t('menus.vehHeader_header'),
         txt = Lang:t('menus.vehHeader_txt'),
         icon = "fa-solid fa-car",
         params = {

91e3a64cc86eac0c6057c8622f18ddf40d3b5f18
Ksnarf <44852455+Ksnarf@users.noreply.github.com>
2022-06-12 00:36:46 -0700

    FAT Agency

---
+++ resources/[vehicle]/JMfat/vehicle_names.lua
@@ -0,0 +1,7 @@
+Citizen.CreateThread(function()
+	AddTextEntry('0x86BDB266', 'FAT')
+	AddTextEntry('0x0EFBE06C', 'Torrence')
+	AddTextEntry('0xEABEC023', 'Buffalo 08')
+	AddTextEntry('0xE4329201', 'Landstalker (SWB)')
+	AddTextEntry('0x97553C28', 'Everon')
+end)

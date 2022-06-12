a6b24afb90631ddbab14f5e130305b48917518c3
Ksnarf <44852455+Ksnarf@users.noreply.github.com>
2022-06-12 13:23:26 -0700

    unfuckup the fuckup
    mh60i, bell360, fibfrogger

--- resources/[vehicle]/bell360/__resource.lua
+++ resources/[vehicle]/bell360/__resource.lua
@@ -1,7 +1,24 @@
-resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
 
-data_file 'HANDLING_FILE' 'handling.meta'
+description "Car Add-on"
 
 files {
-        'handling.meta'
+    'vehicles.meta',
+    'carvariations.meta',
+    'carcols.meta',
+    'handling.meta',
+--    'data/vehiclelayouts.meta',
+--    'data/contentunlocks.meta',
+}
+
+data_file 'HANDLING_FILE' 'handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+data_file 'CARCOLS_FILE' 'carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
+--data_file 'VEHICLE_LAYOUTS_FILE''data/vehiclelayouts.meta'
+-- data_file 'VEHICLE_SHOP_DLC_FILE' 'data/'
+--data_file 'CONTENT_UNLOCKING_META_FILE' 'contentunlocks.meta'
+
+client_script {
+--    'data/vehicle_names.lua'
 }

bdc5b3da4fee2a93ec8bdefc70f521e1b862e29e
Ksnarf <44852455+Ksnarf@users.noreply.github.com>
2022-06-11 22:30:47 -0700

    c63raid resource file

--- resources/[vehicle]/c63raid/__resource.lua
+++ resources/[vehicle]/c63raid/__resource.lua
@@ -6,15 +6,15 @@ resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
 
 -- why are you tageting me ? ʕ•ᴥ•ʔ
 files {
-    'data/**/handling.meta',
-    'data/**/vehicles.meta',
-    'data/**/carvariations.meta',
-    'data/**/carcols.meta',
-    'data/**/vehiclelayouts.meta',
+    'handling.meta',
+    'vehicles.meta',
+    'carvariations.meta',
+    'carcols.meta',
+    'vehiclelayouts.meta',
 }
 
-data_file 'HANDLING_FILE'            'data/**/handling.meta'
-data_file 'VEHICLE_METADATA_FILE'    'data/**/vehicles.meta'
-data_file 'CARCOLS_FILE'             'data/**/carcols.meta'
-data_file 'VEHICLE_VARIATION_FILE'   'data/**/carvariations.meta'
-data_file 'VEHICLE_LAYOUTS_FILE'     'data/**/vehiclelayouts.meta'
+data_file 'HANDLING_FILE'            'handling.meta'
+data_file 'VEHICLE_METADATA_FILE'    'vehicles.meta'
+data_file 'CARCOLS_FILE'             'carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE'   'carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE'     'vehiclelayouts.meta'
